9e0d5c061a933ef895f4eece49623b94004f9409
acnodu <github@acnodu.fr>
2025-07-10 17:13:59 +0200

    Remove MongoDB URI from nuxt.config.ts and log statements from mongoose.js for improved security and organization.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -12,7 +12,6 @@ export default defineNuxtConfig({
     },
   css: ["@/assets/css/custom.css"],
   runtimeConfig: {
-    mongodbUri: `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}/${process.env.ENV}?retryWrites=true&w=majority`,
     public: {
         apiUrl: process.env.NUXT_API_URL || '/api',
         appEnv: process.env.NUXT_APP_ENV || 'prod',
--- server/plugins/mongoose.js
+++ server/plugins/mongoose.js
@@ -1,17 +1,9 @@
 import mongoose from 'mongoose';
 
 export default defineNitroPlugin(async () => {
-    const config = useRuntimeConfig();
-
-    console.log('MONGODB_USERNAME:', process.env.MONGODB_USERNAME);
-    console.log('MONGODB_PASSWORD:', process.env.MONGODB_PASSWORD);
-    console.log('MONGODB_HOST:', process.env.MONGODB_HOST);
-    console.log('ENV:', process.env.ENV);
-    console.log('NUXT_API_URL:', process.env.NUXT_API_URL);
-    console.log('NUXT_APP_ENV:', process.env.NUXT_APP_ENV);
-
     if (mongoose.connection.readyState === 0) {
-        console.log(config.mongodbUri);
-        await mongoose.connect(config.mongodbUri);
+        await mongoose.connect(
+            `mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@${process.env.MONGODB_HOST}/${process.env.ENV}?retryWrites=true&w=majority`
+        );
     }
 });
