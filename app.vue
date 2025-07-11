0bbeba5d746552facc26a6e430397665ada0b17d
acnodu <github@acnodu.fr>
2025-07-11 11:36:14 +0200

    Add Open Graph and Twitter Card meta tags for improved SEO

--- app.vue
+++ app.vue
@@ -4,6 +4,7 @@
 
 <script setup>
 useHead({
+    title: 'Ragondin',
     meta: [
         {
             name: 'description',
@@ -15,6 +16,7 @@ useHead({
             content:
                 'Picnic, réduction Picnic, code promo Picnic, -10%, gratuit, panier Picnic, économie, application réduction',
         },
+        // Open Graph (Facebook, LinkedIn, etc.)
         {
             property: 'og:title',
             content: 'Réduction Picnic de -10% gratuitement',
@@ -31,6 +33,28 @@ useHead({
         {
             property: 'og:type',
             content: 'website',
+        },
+        {
+            property: 'og:url',
+            content: 'https://piknik.home.acnodu.fr',
+        },
+        // Twitter Card
+        {
+            name: 'twitter:card',
+            content: 'summary_large_image',
+        },
+        {
+            name: 'twitter:title',
+            content: 'Réduction Picnic de -10% gratuitement',
+        },
+        {
+            name: 'twitter:description',
+            content:
+                'Obtenez -10% sur votre commande Picnic sans rien payer. Connectez-vous et appliquez la réduction en un clic !',
+        },
+        {
+            name: 'twitter:image',
+            content: '/assets/images/logo.png',
         },
     ],
 });
