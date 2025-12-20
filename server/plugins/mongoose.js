8b864d9fcdacd858c77c5d23687465ddc86d037c
acnodu <github@acnodu.fr>
2025-12-21 00:20:14 +0100

    chore: add logging for environment and config in mongoose plugin

--- server/plugins/mongoose.js
+++ server/plugins/mongoose.js
@@ -4,6 +4,9 @@ import { config, generateConfig } from '../config';
 export default defineNitroPlugin(async () => {
     await generateConfig();
 
+    console.log(process.env.ENV);
+    console.log(config);
+
     if (mongoose.connection.readyState === 0) {
         await mongoose.connect(
             `mongodb+srv://${config.mongoUsername}:${config.mongoPassword}@${config.mongoHost}/${
