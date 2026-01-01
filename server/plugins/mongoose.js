e332c1be68bf16690346d3044bbb129ba4217006
acnodu <github@acnodu.fr>
2026-01-01 21:38:08 +0100

    fix: update MongoDB connection string to include user, pass, and replica set

--- server/plugins/mongoose.js
+++ server/plugins/mongoose.js
@@ -5,9 +5,11 @@ export default defineNitroPlugin(async () => {
     if (mongoose.connection.readyState === 0) {
         await generateConfig();
 
-        await mongoose.connect(
-            `mongodb://${config.mongoUser}:${config.mongoPass}@${config.mongoHost}/${config.mongoUser}
-            }?authSource=admin`
-        );
+        await mongoose.connect(`mongodb://${config.mongoHost}/${config.mongoUser}`, {
+            user: config.mongoUser,
+            pass: config.mongoPass,
+            authSource: 'admin',
+            replicaSet: 'replicaset',
+        });
     }
 });
