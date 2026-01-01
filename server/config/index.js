318f18a60bb10ec897e94ec5392458bb8e87ebf7
acnodu <34336978+acnodu@users.noreply.github.com>
2026-01-02 00:55:24 +0100

    ok

--- server/config/index.js
+++ server/config/index.js
@@ -9,17 +9,15 @@ const vault = vaultFn({
 let config = {};
 
 const generateConfig = async () => {
-    const { default: pkg } = await import('./../../package.json', { with: { type: 'json' } });
-
     const login = await vault.approleLogin({
         role_id: '8dc52e67-feab-5e6d-903b-33fcd4e28671',
         secret_id: '65c641c0-2ed5-b6f7-53d4-5629db54bf0a',
     });
 
     vault.token = login.auth.client_token;
-
+    console.log(`docker/data/picnic/${process.env.ENV || 'dev'}`.toLowerCase())
     const { data } = await vault.read(
-        `docker/data/${pkg.name}/${process.env.ENV || 'dev'}`.toLowerCase()
+        `docker/data/ragondin/${process.env.ENV || 'dev'}`.toLowerCase()
     );
 
     config = data.data;
