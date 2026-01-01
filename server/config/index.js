6b01529f364f212cf8f67bac975bf0c0660d5b68
acnodu <34336978+acnodu@users.noreply.github.com>
2026-01-02 00:58:16 +0100

    go

--- server/config/index.js
+++ server/config/index.js
@@ -2,22 +2,27 @@ import vaultFn from 'node-vault';
 
 const vault = vaultFn({
     apiVersion: 'v1',
-    endpoint: 'http://192.168.23.49:8200',
+    endpoint: 'http://vault.priv.acnodu.fr',
 });
 
 // eslint-disable-next-line import/no-mutable-exports
 let config = {};
 
 const generateConfig = async () => {
+    const { default: pkg } = await import('./../../package.json', { with: { type: 'json' } });
+
     const login = await vault.approleLogin({
         role_id: '8dc52e67-feab-5e6d-903b-33fcd4e28671',
         secret_id: '65c641c0-2ed5-b6f7-53d4-5629db54bf0a',
     });
 
+    console.log(
+        `docker/data/${pkg.name}/${process.env.APP_ENV || 'dev'}`.toLowerCase())
+
     vault.token = login.auth.client_token;
-    console.log(`docker/data/picnic/${process.env.ENV || 'dev'}`.toLowerCase())
+
     const { data } = await vault.read(
-        `docker/data/ragondin/${process.env.ENV || 'dev'}`.toLowerCase()
+        `docker/data/${pkg.name}/${process.env.APP_ENV || 'dev'}`.toLowerCase()
     );
 
     config = data.data;
