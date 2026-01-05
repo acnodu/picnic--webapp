c5a31cf668a6031b77373dcefa49e125fe513f77
acnodu <34336978+acnodu@users.noreply.github.com>
2026-01-05 16:10:52 +0100

    rrr

--- server/api/mfa/verify.post.js
+++ server/api/mfa/verify.post.js
@@ -14,7 +14,6 @@ export default defineEventHandler(async (event) => {
             createError({ statusCode: 400, statusMessage: 'Invalid input data' })
         );
     }
-    console.log('ok')
 
     return await mfaVerify(event);
 });
