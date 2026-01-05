b3bef3ac313e8db6296fc36c68655ab1e459c36a
acnodu <github@acnodu.fr>
2026-01-05 23:13:34 +0100

    add error handling for Preline library import in Nuxt plugin

--- plugins/preline.client.js
+++ plugins/preline.client.js
@@ -1,9 +1,22 @@
-import('preline/dist');
-
 export default defineNuxtPlugin(() => {
     const router = useRouter();
 
     router.afterEach(async () => {
-        setTimeout(() => window.HSStaticMethods.autoInit());
+        try {
+            await import('preline/dist');
+        } catch (e) {
+            console.error('Preline library is not available.');
+            console.error(e);
+        }
+
+        setTimeout(() => {
+            if (
+                typeof window !== 'undefined' &&
+                window.HSStaticMethods &&
+                typeof window.HSStaticMethods.autoInit === 'function'
+            ) {
+                window.HSStaticMethods.autoInit();
+            }
+        }, 0);
     });
 });
